2559ec342e33a095860ccc4e4365b00272a80a8b
Saptarshi <roni.bagchi@gmail.com>
2021-08-10 21:50:10 +0530

    Added another dish

--- stuff.js
+++ stuff.js
@@ -1,23 +1,24 @@
 const recommend = require("collaborative-filter");
 
-const itemsArray = ["Mexican", "Chinese", "Indian"];
+const itemsArray = ["Mexican", "Chinese", "Indian", "Continental"];
 
-const usersArray = ["Saptarshi", "Deep", "Dipankar"];
+const usersArray = ["Customer 1", "Customer 2", "Customer 3"];
 
 // the rows are the users and the columns are the items and whether they like it or not
 const ratings = [
-  [1, 1, 1],
-  [1, 0, 1],
-  [1, 0, 0],
+  [1, 1, 1, 0],
+  [1, 0, 1, 1],
+  [1, 0, 0, 1],
 ];
 
 //const result = recommend.cFilter(ratings, 2);
 
 usersArray.map((el, index) => {
   const result = recommend.cFilter(ratings, index);
-
+  console.log(`For ${el},`);
   if (result.length > 0)
-    console.log(`For ${el}, the best item would be : ${itemsArray[result[0]]}`);
-  console.log(result);
+    result.map((val) => console.log(` ${itemsArray[val]}`));
+
+  // console.log(result);
 });
 //console.log(result);
